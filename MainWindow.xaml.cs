7a3dd2b1278a91aa00a0e9918715b2535c5f9535
sarrikzhdyn <sarrikzhdyn@gmail.com>
2025-10-27 12:47:36 +0300

    Add files via upload
    Добавили модуль 8

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using PasswordManager.ViewModels;
+
+namespace PasswordManager
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new MainViewModel();
+            this.DataContext = _vm;
+        }
+
+        private void MasterPassBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (_vm != null)
+                _vm.MasterPassword = MasterPassBox.Password;
+        }
+    }
+}
